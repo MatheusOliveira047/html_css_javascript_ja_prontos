f0e433cc9d51d0a3ba11ad243d32d8ca3862abb2
MatheusOliveira047 <ludsonmatheus@gmail.com>
2022-04-17 00:15:42 -0300

    limpando o codigo

--- menu_responsivo.js
+++ menu_responsivo.js
@@ -1,14 +1,16 @@
+//SELETORES
 let menuResponsivo = document.querySelector(".header")
 let menuHambuguer = document.querySelector(".menu")
 let fecharMenuAncoras = document.querySelectorAll(".nav-list li a")
-menuHambuguer.addEventListener("click",()=>{
-    menuHambuguer.classList.toggle("active")
-    menuResponsivo.classList.toggle("active") 
-})
+
+//ADICIONANDO OS EVENTOS DE CLICK 
+menuHambuguer.addEventListener("click",menu)
 fecharMenuAncoras.forEach((ancoras)=>{
-    ancoras.addEventListener('click',()=>{
-        menuHambuguer.classList.toggle("active")
-        menuResponsivo.classList.toggle("active")
-        ancoras.classList.toggle("active")
-    })   
+    ancoras.addEventListener('click',menu)   
 })
+
+//DECLARANDO A FUNÇÃO
+function menu(){
+    menuHambuguer.classList.toggle("active")
+    menuResponsivo.classList.toggle("active")    
+}
